de94fdbc460b4deea72802849afa5cbf1d7f4bf0
Michael Santos <michael.shel96@gmail.com>
2019-12-23 18:07:12 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace imc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
